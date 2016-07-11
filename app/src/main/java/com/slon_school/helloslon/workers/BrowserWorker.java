8828ef8366eb96b78dab914c205461961a7654ca
Michail <apd817376@gmail.com>
2016-07-11 10:11:18 +0300

    BrowseWorker
    afafafa

--- app/src/main/java/com/slon_school/helloslon/workers/BrowserWorker.java
+++ app/src/main/java/com/slon_school/helloslon/workers/BrowserWorker.java
@@ -8,33 +8,34 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.slon_school.helloslon.core.Key;
 import com.slon_school.helloslon.core.Worker;
 
 import java.util.ArrayList;
 
 public class BrowserWorker extends Worker {
-    private ArrayList<String> keys;
+    private ArrayList<Key> keys;
     private boolean isContinue;
 
     public BrowserWorker(Activity activity) {
         super(activity);
 
         isContinue = false;
-        keys = new ArrayList<String>();
-        keys.add("найди");
-        keys.add("поиск");
-        keys.add("интернет");
-        keys.add("браузер");
-        keys.add("Яндекс");
-        keys.add("Гугл");
-        keys.add("искать");
+        keys = new ArrayList<Key>();
+        keys.add(new Key("найди"));
+        keys.add(new Key("поиск"));
+        keys.add(new Key("интернет"));
+        keys.add(new Key("браузер"));
+        keys.add(new Key("Яндекс"));
+        keys.add(new Key("Гугл"));
+        keys.add(new Key("искать"));
         //etc
 
 
     }
 
     @Override
-    public ArrayList<String> getKeys() {
+    public ArrayList<Key> getKeys() {
         return keys;
     }
 
@@ -42,10 +43,11 @@ public class BrowserWorker extends Worker {
     public boolean isContinue(){return false;}
 
     @Override
-    public String doWork(ArrayList<String> arguments) {
+    public String doWork(ArrayList<Key> keys,ArrayList<String> arguments) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("http://www.yandex.ru?q="+arguments.get(0)));
+        intent.setData(Uri.parse("https://www.google.ru/search?q="+arguments.get(0)));
         activity.startActivity(intent);
         return "";
 
