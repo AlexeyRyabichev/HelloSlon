f5d9b4e6788619d957c4ba4a25a22c672a8afd35
Michail <apd817376@gmail.com>
2016-07-14 09:37:32 +0300

    12345
    12345

--- app/src/main/java/com/slon_school/helloslon/workers/PhoneWorker.java
+++ app/src/main/java/com/slon_school/helloslon/workers/PhoneWorker.java
@@ -1,7 +1,56 @@
 package com.slon_school.helloslon.workers;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.slon_school.helloslon.core.Key;
+import com.slon_school.helloslon.core.Response;
+import com.slon_school.helloslon.core.Worker;
+
+import java.util.ArrayList;
 /**
  * Created by Mike on 12.07.2016.
  */
-public class PhoneWorker {
+public class PhoneWorker extends Worker {
+    private ArrayList<Key> keys;
+    private boolean isContinue;
+
+    public PhoneWorker(Activity activity) {
+        super(activity);
+
+        isContinue = false;
+        keys = new ArrayList<Key>();
+        keys.add(new Key("позвони"));
+        keys.add(new Key("набери"));
+        keys.add(new Key("позвонить"));
+        keys.add(new Key("набрать"));
+        keys.add(new Key("дозвонись"));
+
+
+        //etc
+    }
+
+    @Override
+    public ArrayList<Key> getKeys() {
+        return keys;
+    }
+
+    @Override
+    public boolean isLoop(){return false;}
+
+    @Override
+    public Response doWork(ArrayList<Key> result, Key arguments) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:8(903)635-88-99"));
+        activity.startActivity(intent);
+        return new Response("", false);
+    }
+
+
 }
