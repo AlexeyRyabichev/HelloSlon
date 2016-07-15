ba9e818ebd80fad78272941dfd6b3da070c5dacd
gotoSlon <odp230s@gmail.com>
2016-07-15 09:44:48 +0300

    .

--- app/src/main/java/com/slon_school/helloslon/core/Core.java
+++ app/src/main/java/com/slon_school/helloslon/core/Core.java
@@ -7,12 +7,9 @@ import com.slon_school.helloslon.workers.AlarmWorker;
 import com.slon_school.helloslon.workers.BashOrgRandomQuote;
 import com.slon_school.helloslon.workers.BrowserWorker;
 import com.slon_school.helloslon.workers.FateBallWorker;
-<<<<<<< HEAD
 import com.slon_school.helloslon.workers.HelpWorker;
 import com.slon_school.helloslon.workers.SMSSendWorker;
-=======
 import com.slon_school.helloslon.workers.SMSWorker;
->>>>>>> develop
 import com.slon_school.helloslon.workers.TestWorker;
 import com.slon_school.helloslon.workers.TownWorker;
 
@@ -40,13 +37,10 @@ public class Core {
 		workers.add(new SMSSendWorker(activity));
 		workers.add(new AlarmWorker(activity));
 		workers.add(new TownWorker(activity));
-<<<<<<< HEAD
 		workers.add(new BashOrgRandomQuote(activity));
 		workers.add(new HelpWorker(activity));
-
-=======
 		workers.add(new SMSWorker(activity));
->>>>>>> develop
+
 		currentWorker = idNone;
 	}
 	
