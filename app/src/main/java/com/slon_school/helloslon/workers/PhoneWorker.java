be91fb4f27bcfa72b5e9f3e46efe177ca968b376
Michail <apd817376@gmail.com>
2016-07-16 10:00:03 +0300

    PhoneWorker

--- app/src/main/java/com/slon_school/helloslon/workers/PhoneWorker.java
+++ app/src/main/java/com/slon_school/helloslon/workers/PhoneWorker.java
@@ -2,37 +2,30 @@ package com.slon_school.helloslon.workers;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
 
+import com.slon_school.helloslon.R;
+import com.slon_school.helloslon.core.Helper;
 import com.slon_school.helloslon.core.Key;
 import com.slon_school.helloslon.core.Response;
 import com.slon_school.helloslon.core.Worker;
 
 import java.util.ArrayList;
+
 /**
- * Created by Mike on 12.07.2016.
+ * Created by I. Dmitry on 12.07.2016.
  */
 public class PhoneWorker extends Worker {
-    private ArrayList<Key> keys;
-    private boolean isContinue;
+    private ArrayList<Key> keys = new ArrayList<Key>();
+    private final static boolean finishSession = false;
 
-    public PhoneWorker(Activity activity) {
+    public PhoneWorker (Activity activity) {
         super(activity);
-
-        isContinue = false;
-        keys = new ArrayList<Key>();
         keys.add(new Key("позвони"));
-        keys.add(new Key("набери"));
         keys.add(new Key("позвонить"));
+        keys.add(new Key("дозвнись"));
+        keys.add(new Key("набери"));
         keys.add(new Key("набрать"));
-        keys.add(new Key("дозвонись"));
-
-
-        //etc
+        keys.add(new Key("звонок"));
     }
 
     @Override
@@ -41,16 +34,25 @@ public class PhoneWorker extends Worker {
     }
 
     @Override
-    public boolean isLoop(){return false;}
+    public boolean isLoop() { return false; }
 
     @Override
-    public Response doWork(ArrayList<Key> result, Key arguments) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_CALL);
-        intent.setData(Uri.parse("tel:8(903)635-88-99"));
-        activity.startActivity(intent);
-        return new Response("", false);
+    public Response doWork(ArrayList<Key> keys, Key arguments) {
+        if (arguments.get().size() == 0) {
+            return new Response("Повтори номер, пожалуйста.",finishSession);
+        }
+        String phoneNumber = arguments.toString();
+
+        if (Helper.isValidMobilePhoneNumber(phoneNumber)) {
+            startActivity(phoneNumber);
+            return new Response("Выполняю звонок наномер" + " " + phoneNumber, finishSession);
+        } else {
+            return new Response("Не могу найти такой номер" + " " + phoneNumber + "?", finishSession);
+        }
     }
 
-
+    private void startActivity(final String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+        activity.startActivity(intent);
+    }
 }
