fff009e5f50ea811d6a062cdb3e91956e76e7ed3
Tayrinn <aver.kev@gmail.com>
2016-07-17 12:34:31 +0300

    fix dilbert

--- app/src/main/java/com/slon_school/helloslon/workers/DilbertWorker.java
+++ app/src/main/java/com/slon_school/helloslon/workers/DilbertWorker.java
@@ -13,24 +13,55 @@ import java.util.ArrayList;
  */
 public class DilbertWorker extends Worker
 {
-
+    private String link1;
+    private String link2;
+    private boolean isLinkGot;
+    private ArrayList<Key> keys = new ArrayList<Key>();
+    private int PicPointer;
 
     public DilbertWorker( Activity activity ) {
         super( activity );
+        keys.add(new Key("дилберт"));
+        keys.add(new Key("гилберт"));
     }
 
     @Override
     public ArrayList<Key> getKeys() {
-        return null;
+        return keys;
     }
 
     @Override
     public boolean isLoop() {
-        return false;
+        return true;
     }
 
     @Override
     public Response doWork( ArrayList<Key> keys, Key arguments ) {
+        if(keys.size() != 0) {
+            // начать сканировать сайт
+            //http://dilbertru.blogspot.ru/search/label/%D0%94%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D1%82
+            //<li><a href='http://dilbertru.blogspot.ru
+
+            //if(keys.get(0).get())
+        }
+
+
+        if(arguments.get().size() != 0){
+            // работать с картинками
+            //view-source:http://dilbertru.blogspot.ru/2016/07/20160716.html
+            //<img src="http://assets.amuniversal.com
+
+            String comand = arguments.get().get(0);
+            if(comand.equals("следующий") || comand.equals("еще") || comand.equals("да")) {
+
+            }
+            if(comand.equals("хватит")) {
+                return new Response("Действительно хватит", false);
+            }
+            if(comand.equals("нет")) {
+                return new Response("Ну ладно", false);
+            }
+        }
         return null;
     }
 }
