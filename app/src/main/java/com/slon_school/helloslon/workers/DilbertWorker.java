75edbb99cb4db44fcd79ce412f602df25e4084f9
Tayrinn <aver.kev@gmail.com>
2016-07-18 18:42:39 +0300

    fix dilbert

--- app/src/main/java/com/slon_school/helloslon/workers/DilbertWorker.java
+++ app/src/main/java/com/slon_school/helloslon/workers/DilbertWorker.java
@@ -1,12 +1,19 @@
 package com.slon_school.helloslon.workers;
 
 import android.app.Activity;
+import android.util.TypedValue;
 
+import com.slon_school.helloslon.R;
 import com.slon_school.helloslon.core.Key;
 import com.slon_school.helloslon.core.Response;
 import com.slon_school.helloslon.core.Worker;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by Volha on 17.07.2016.
@@ -18,12 +25,16 @@ public class DilbertWorker extends Worker
     private boolean isLink1Got;
     private boolean isLink2Got;
     private ArrayList<Key> keys = new ArrayList<Key>();
-    private int PicPointer;
+    private int picPointer;
+
+
+    ArrayList<String> linkArr;
 
     public DilbertWorker( Activity activity ) {
         super( activity );
         keys.add(new Key("дилберт"));
         keys.add(new Key("гилберт"));
+        picPointer = 0;
     }
 
     @Override
@@ -43,7 +54,12 @@ public class DilbertWorker extends Worker
             //http://dilbertru.blogspot.ru/search/label/%D0%94%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D1%82
             //<li><a href='http://dilbertru.blogspot.ru
 
-            //if(keys.get(0).get())
+            if(keys.get(0).toString().equals("гилберт")) {
+                return new Response("Ты хочешь сказать \"Дилберт\"?", true);
+            }
+            else {
+                picPointer = 1;
+            }
         }
 
 
@@ -55,6 +71,8 @@ public class DilbertWorker extends Worker
             String comand = arguments.get().get(0);
             if(comand.equals("следующий") || comand.equals("еще") || comand.equals("да")) {
 
+                picPointer++;
+                return null;
             }
             if(comand.equals("хватит")) {
                 return new Response("Действительно хватит", false);
@@ -67,7 +85,38 @@ public class DilbertWorker extends Worker
     }
 
 
+    private ArrayList<String> fillLinks() {
+        ArrayList<String> res = new ArrayList<String>();
 
+        boolean isLinkGot;
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    isLinkGot = getLink("http://dilbertru.blogspot.ru/search/label/%D0%94%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D1%82", res);//----------------------------------
+                    countDownLatch.countDown();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (isLinkGot) {
+            return res;
+        } else {
+            //--------------------------------------------------------------------------------
+            return null;
+        }
+
+        return res;
+    }
 
 
 /*
@@ -101,33 +150,25 @@ public class DilbertWorker extends Worker
     }
 
 
-    public boolean getLink(boolean level, String URLStr) throws Exception {
-        String line;
-        /*Random random = new Random();
+
+*/
+public boolean getLink(String URLStr, ArrayList<String> res) throws Exception {//----------------------------------------------------------------
+    String line;
+        Random random = new Random();
         Integer tmp = random.nextInt(551);
 
         URL url = new URL(URLStr);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(),
-                activity.getString( R.string.cp1251)));
-        while (true) {
-            line = reader.readLine();
-            if (line == null) {
-                break;
-            }
-            if (line.contains("  <p><img src=")) {
-                if(level) {
-                    quote = washLink1( line );
+    BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(),
+            activity.getString( R.string.cp1251)));
+        while ((line = reader.readLine()) != null) {
+            if (line.contains("<li><a href='http://dilbertru.blogspot.ru")) {
+                    res.add(washLink1( line ));
                 }
-                else {
-                    quote = washLink2(line);
-                }
-
-                return true;
             }
+        return true;
         }
         return false;
     }
-*/
     private String washLink1( String line ) {
         line = line.replace("  <p><img src=\"","");
         line = line.replaceAll("\"(.)*","");
