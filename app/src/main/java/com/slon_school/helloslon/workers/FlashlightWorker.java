c9eb6cb4cca5fe081ea1c9dadc5213d12fc21f1e
gotoSlon <odp230s@gmail.com>
2016-07-22 19:47:16 +0300

    Fixes

--- app/src/main/java/com/slon_school/helloslon/workers/FlashlightWorker.java
+++ app/src/main/java/com/slon_school/helloslon/workers/FlashlightWorker.java
@@ -6,11 +6,8 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
-<<<<<<< HEAD
-=======
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
->>>>>>> develop
 import android.widget.Toast;
 
 import com.slon_school.helloslon.R;
@@ -25,19 +22,24 @@ import java.util.ArrayList;
 import static com.slon_school.helloslon.core.Helper.isDecimalNumber;
 import static com.slon_school.helloslon.core.Helper.string2long;
 
-public class FlashlightWorker extends Worker implements Helper.additionalInterface{
+public class FlashlightWorker extends Worker implements Helper.additionalInterface {
     private long time;
     ArrayList<Key> keys = new ArrayList<>();
+
     public FlashlightWorker(Activity activity) {
         super(activity, "фонарик");
         keys.add(new Key(activity.getString(R.string.flashlight_keyword0)));
     }
 
     @Override
-    public ArrayList<Key> getKeys() { return keys; }
+    public ArrayList<Key> getKeys() {
+        return keys;
+    }
 
     @Override
-    public boolean isLoop() { return false; }
+    public boolean isLoop() {
+        return false;
+    }
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
@@ -45,23 +47,19 @@ public class FlashlightWorker extends Worker implements Helper.additionalInterfa
         if (arguments.contains(new Key(activity.getString(R.string.help0))) || arguments.contains(new Key(activity.getString(R.string.help1)))) {
             return getHelp();
         }
-
-//        boolean hasAccessibleCamera; //TODO check new features
         final long MULTIPLE = 1000;
         final long DEFAULT_TIME = 60;
         String sTime = arguments.toString();
-        Toast.makeText(activity,sTime,Toast.LENGTH_LONG).show();
+        Toast.makeText(activity, sTime, Toast.LENGTH_LONG).show();
         if (isDecimalNumber(sTime.trim())) {
             time = string2long(sTime) * MULTIPLE;
         } else {
             time = DEFAULT_TIME * MULTIPLE;
         }
-        // Toast.makeText(activity,"" + Camera.getNumberOfCameras(),Toast.LENGTH_LONG).show();
         if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             activity.requestPermissions(new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
             return new Response("Попробуйте ещё раз", false);
         }
-        if (Camera.getNumberOfCameras() > 0) {
             final Camera CAMERA = Camera.open();
             Parameters parameters = CAMERA.getParameters();
             parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
@@ -80,15 +78,11 @@ public class FlashlightWorker extends Worker implements Helper.additionalInterfa
                 }
             };
             thread.start();
-            hasAccessibleCamera = true;
-        } else {
-            hasAccessibleCamera = false;
+            return new Response("", FINISH_SESSION);
         }
-        return new Response(hasAccessibleCamera ? "" : "Камера уже используется",FINISH_SESSION);
-    }
 
-    @Override
-    public Response getHelp() {
-        return new HelpMan(R.raw.flashlight_help, activity).getHelp();
+        @Override
+        public Response getHelp () {
+            return new HelpMan(R.raw.flashlight_help, activity).getHelp();
+        }
     }
-}
